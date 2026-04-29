d88be28c45b08e360c8bb22ed1556af9e07d55ed
maswin18 <280404051+maswin18@users.noreply.github.com>
2026-04-29 12:25:55 -0400

    add PUT update product endpoint

--- app.py
+++ app.py
@@ -12,3 +12,20 @@ def get_products():
         {"id": 1, "name": "Shoes", "qty": 100},
         {"id": 2, "name": "T-Shirt", "qty": 50}
     ]
+
+from pydantic import BaseModel
+
+class Product(BaseModel):
+    id: int
+    name: str
+    qty: int
+
+Products = [
+    {"id": 1, "name": "Shoes", "qty": 100},
+    {"id": 2, "name": "T-Shirt", "qty": 50}
+]
+
+@app.post("/products")
+def add_product(product: Product):
+    Products.append(product.dict())
+    return {"message": "Product added", "data": product}
