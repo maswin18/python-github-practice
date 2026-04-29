97f18be6e6b5d62df5611889592197a08eba0966
maswin18 <280404051+maswin18@users.noreply.github.com>
2026-04-29 11:14:59 -0400

    add products endpoint

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
-print("Hello GitHub!")
-print("Second update from Maswin")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Hello Maswin, this is your first API!"}
+
+@app.get("/products")
+def get_products():
+    return [
+        {"id": 1, "name": "Shoes", "qty": 100},
+        {"id": 2, "name": "T-Shirt", "qty": 50}
+    ]
