92740419ef170fdd44cdd0b73728e0c6eeab7fff
maswin18 <280404051+maswin18@users.noreply.github.com>
2026-04-30 10:58:53 -0400

    add logging to sync client for traceability and debugging

--- client.py
+++ client.py
@@ -1,5 +1,12 @@
 import requests
 import time
+import logging
+
+logging.basicConfig(
+    filename="sync.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 BASE_URL = "http://127.0.0.1:8000"
 
@@ -10,12 +17,23 @@ login_data = {
 }
 
 def get_token():
-    response = requests.post(f"{BASE_URL}/login", json=login_data)
-    return response.json().get("access_token")
+    try:
+        response = requests.post(f"{BASE_URL}/login", json=login_data)
+        return response.json().get("access_token")
+    except:
+        print("Server not available...")
+        return None
 
 def sync_job():
+    logging.info("Starting sync job")
+
     token = get_token()
 
+    if not token:
+        logging.error("Login failed - no token")
+        print("Skipping...")
+        return
+    
     headers = {
         "Authorization": f"Bearer {token}"
     }
@@ -25,22 +43,35 @@ def sync_job():
         {"id": 4, "name": "Bag", "qty": 20}
     ]
 
-    sync_response = requests.post(f"{BASE_URL}/sync", json=sync_data, headers=headers)
+    logging.info(f"Sending sync data: {sync_data}")
 
-    print("Sync:", sync_response.json())
+    try:
+        sync_response = requests.post(f"{BASE_URL}/sync", json=sync_data, headers=headers)
+        logging.info(f"Sync response: {sync_response.json()}")
+    except Exception as e:
+        logging.error(f"Sync failed: {e}")
+        return
 
     products_response = requests.get(f"{BASE_URL}/products", headers=headers)
 
     if products_response.status_code == 401:
-        print("Token expired, retrying...")
+        logging.warning("Token expired, retrying login")
+
         token = get_token()
         headers["Authorization"] = f"Bearer {token}"
+
         products_response = requests.get(f"{BASE_URL}/products", headers=headers)
 
-    print("Products:", products_response.json())
+    logging.info(f"Products after sync: {products_response.json()}")
+
+    print ("Sync OK")
 
 # Run every 10 seconds
-while True:
-    print("\n--- Running sync job ---")
-    sync_job()
-    time.sleep(10)
+try:
+    while True:
+        print("\n--- Running sync job ---")
+        sync_job()
+        time.sleep(10)
+except:
+    print("\nStopped by user.")
+
